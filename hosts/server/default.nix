0960f63f59b4fd10e224ea534232e97da12c71fb
Jylhis <markus@jylhis.com>
2025-02-15 21:37:29 +0100

    format users field

--- hosts/server/default.nix
+++ hosts/server/default.nix
@@ -53,10 +53,19 @@ in
     };
   };
 
-  users.users.root.password = "root";
-  users.users.root.openssh.authorizedKeys.keys = [
-    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIK4zXcaYT+RTxvAjUjE3B33kwwxCOo4ApI4diLnajbUT"
-  ];
+  users = {
+
+    groups = {
+    };
+
+    users = {
+
+      root.password = "root"; # FIXME
+      root.openssh.authorizedKeys.keys = [
+        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIK4zXcaYT+RTxvAjUjE3B33kwwxCOo4ApI4diLnajbUT"
+      ];
+    };
+  };
 
   # Bootloader.
   boot = {
