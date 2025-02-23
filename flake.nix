ca8687e3bed369ffbe5234b8d1af74860d21a3b8
Jylhis <markus@jylhis.com>
2025-02-23 13:30:17 +0100

    Formatting

--- flake.nix
+++ flake.nix
@@ -189,6 +189,9 @@
 
         packages = {
 
+          # Cross compilation
+          #hello-cross = nixpkgs.legacyPackages.${system}.pkgsCross.aarch64-multiplatform.hello;
+
           emacs-markus = pkgs.callPackage ./packages/emacs-markus {
             emacs = pkgs.emacs;
           };
@@ -210,23 +213,23 @@
                 runtimeInputs = [
                   pkgs.dconf2nix
                   pkgs.gnused
-		  pkgs.nixfmt-rfc-style
+                  pkgs.nixfmt-rfc-style
                 ];
                 bashOptions = [
                   "errexit"
                   "pipefail"
                 ];
-		# sed command is due to this: https://github.com/nix-community/dconf2nix/issues/112
+                # sed command is due to this: https://github.com/nix-community/dconf2nix/issues/112
                 text = ''
-                  if [ -z "$1" ]; then
-                      echo "Usage: nix run .#dconf-dump path/to/output-file.nix"
-                      exit 1
-                  fi
+                                    if [ -z "$1" ]; then
+                                        echo "Usage: nix run .#dconf-dump path/to/output-file.nix"
+                                        exit 1
+                                    fi
 
-                  dconf dump / \
-                  | sed "/mru-sources/d" \
-                  | dconf2nix \
-		  | nixfmt > "$1"
+                                    dconf dump / \
+                                    | sed "/mru-sources/d" \
+                                    | dconf2nix \
+                  		  | nixfmt > "$1"
                 '';
               }
             );
