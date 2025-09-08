e364236fc04a06ca7d975b41a78580df69055fd2
Jylhis <Jylhis@users.noreply.github.com>
2025-09-08 10:47:19 +0000

    Sync files from source repository

--- modules/home/hyprland.nix
+++ modules/home/hyprland.nix
@@ -1,3 +1,11 @@
+# TODO:
+# - Lock screen block background
+# - Monitor management
+# - Keybindings and workspace management (per monitor?)
+# - Darkmode/lightmode
+# - Autostart: 1password
+# - windows to fix:
+#   - Firefox calendar detail popup
 {
   lib,
   config,
@@ -218,7 +226,7 @@
           # Basic applications
           "$mod, Return, exec, kitty"
           "$mod, B, exec, brave"
-          "$mod, M, exec, emacs"
+          "$mod, M, exec, emacsclient -c -a emacs"
           "$mod, T, exec, kitty"
           "$mod, E, exec, dolphin"
           "$mod, G, exec, emacsclient -cF '((visibility . nil))' -e '(emacs-run-launcher)'"
@@ -234,51 +242,33 @@
           "$mod, P, pseudo"
           "$mod, J, togglesplit"
 
-          # Window focus with arrow keys
+          # Window focus - matching Plasma's Meta+Alt+Arrow
           "$mod, Left, movefocus, l"
           "$mod, Right, movefocus, r"
           "$mod, Up, movefocus, u"
           "$mod, Down, movefocus, d"
 
-          # Window focus - matching Plasma's Meta+Alt+Arrow
-          "$mod ALT, Left, movefocus, l"
-          "$mod ALT, Right, movefocus, r"
-          "$mod ALT, Up, movefocus, u"
-          "$mod ALT, Down, movefocus, d"
-
           # Window tiling - matching Plasma's Meta+Arrow
-          "$mod, Left, movewindow, l"
-          "$mod, Right, movewindow, r"
-          "$mod, Up, movewindow, u"
-          "$mod, Down, movewindow, d"
+          "$mod ALT, Left, movewindow, l"
+          "$mod ALT, Right, movewindow, r"
+          "$mod ALT, Up, movewindow, u"
+          "$mod ALT, Down, movewindow, d"
 
           # Window maximize/minimize
           "$mod, Page_Up, fullscreen, 0" # Maximize Window
-          "$mod, Page_Down, movetoworkspacesilent, special" # Window Minimize equivalent
+          # "$mod, Page_Down, movetoworkspacesilent, special" # Window Minimize equivalent
 
           # Workspace switching
           "$mod, 1, workspace, 1"
           "$mod, 2, workspace, 2"
           "$mod, 3, workspace, 3"
           "$mod, 4, workspace, 4"
-          "$mod, 5, workspace, 5"
-          "$mod, 6, workspace, 6"
-          "$mod, 7, workspace, 7"
-          "$mod, 8, workspace, 8"
-          "$mod, 9, workspace, 9"
-          "$mod, 0, workspace, 10"
 
           # Move windows to workspaces
           "$mod SHIFT, 1, movetoworkspace, 1"
           "$mod SHIFT, 2, movetoworkspace, 2"
           "$mod SHIFT, 3, movetoworkspace, 3"
           "$mod SHIFT, 4, movetoworkspace, 4"
-          "$mod SHIFT, 5, movetoworkspace, 5"
-          "$mod SHIFT, 6, movetoworkspace, 6"
-          "$mod SHIFT, 7, movetoworkspace, 7"
-          "$mod SHIFT, 8, movetoworkspace, 8"
-          "$mod SHIFT, 9, movetoworkspace, 9"
-          "$mod SHIFT, 0, movetoworkspace, 10"
 
           # Special workspace (scratchpad)
           "$mod, S, togglespecialworkspace, magic"
@@ -334,12 +324,6 @@
           "2, monitor:eDP-1"
           "3, monitor:eDP-1"
           "4, monitor:eDP-1"
-          "5, monitor:eDP-1"
-          "6, monitor:eDP-1"
-          "7, monitor:eDP-1"
-          "8, monitor:eDP-1"
-          "9, monitor:eDP-1"
-          "10, monitor:eDP-1"
         ];
 
         # Environment variables
