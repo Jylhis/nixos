1354d7d007ec2fe28fd8b6fc857533424f8d7193
Markus Jylhänkangas <markus@jylhis.com>
2025-09-23 21:49:52 +0200

    plasma

--- modules/nixos/desktop/plasma.nix
+++ modules/nixos/desktop/plasma.nix
@@ -2,56 +2,39 @@
   lib,
   config,
   pkgs,
-  flake,
   ...
 }:
-let
-  inherit (flake) inputs;
-  mapListToAttrs =
-    m: f:
-    lib.listToAttrs (
-      map (name: {
-        inherit name;
-        value = f name;
-      }) m
-    );
-in
 {
 
   imports = [ ../users.nix ];
-  qt.enable = true;
-  services = {
-    desktopManager.plasma6 = {
-      enable = true;
-    };
-    displayManager = {
-      sddm = {
-        enable = true;
-        wayland.enable = true;
-        settings.General.DisplayServer = "wayland";
+  config = lib.mkIf config.services.desktopManager.plasma6.enable {
+    qt.enable = true;
+    services = {
+      displayManager = {
+        sddm = {
+          enable = true;
+          wayland.enable = true;
+          settings.General.DisplayServer = "wayland";
+        };
       };
     };
-  };
-  environment.systemPackages = with pkgs.kdePackages; [
-    filelight
-    partitionmanager
-    ksystemlog
-    isoimagewriter
-    skanlite
-    skanpage
-    kcachegrind
-    falkon
-    arianna
-    francis
-    calligra
-    kalk
-    kcalc
-    kalgebra
-  ];
+    environment.systemPackages = with pkgs.kdePackages; [
+      filelight
+      partitionmanager
+      ksystemlog
+      isoimagewriter
+      skanlite
+      skanpage
+      kcachegrind
+      falkon
+      arianna
+      francis
+      calligra
+      kalk
+      kcalc
+      kalgebra
+    ];
 
-  home-manager = lib.mkIf config.home-manager.enable {
-    users = mapListToAttrs config.myusers (_name: {
-      imports = [ inputs.plasma-manager.homeModules.plasma-manager ];
-    });
+    # FIXME: homeModule plasma is dependent on this
   };
 }
